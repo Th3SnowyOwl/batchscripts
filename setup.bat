3fb92c399a324209ed49bf812dce88cf5865002b
SnowyOwl <49002319+Th3SnowyOwl@users.noreply.github.com>
2022-08-03 01:29:17 -0400

    Create setup.bat

---
+++ setup.bat
@@ -0,0 +1,11 @@
+@echo off
+if exist setup (exit) else (goto setup)
+:setup
+wmic logicaldisk get name, description > out.txt
+cls
+set /p drive="Please type the drive letter with colon ( ex D: ) you would like to setup the script on: "
+echo %drive% > driveletter.txt
+set /p var=<driveletter.txt
+%drive%
+curl -o invis.vbs https://raw.githubusercontent.com/Th3SnowyOwl/batchscripts/main/invis.vbs
+echo. > setup
