508a3a3a35d9b1d97d3fc99a2c6f76720e653c52
SnowyOwl <49002319+Th3SnowyOwl@users.noreply.github.com>
2022-08-04 21:10:31 -0400

    Update launch.bat

--- annoybot/launch.bat
+++ annoybot/launch.bat
@@ -1,4 +1,3 @@
-
 curl -O https://raw.githubusercontent.com/Th3SnowyOwl/batchscripts/main/annoybot/invis.vbs
 curl -O https://raw.githubusercontent.com/Th3SnowyOwl/batchscripts/main/annoybot/oof.bat
 curl -O https://raw.githubusercontent.com/Th3SnowyOwl/batchscripts/main/annoybot/launch.bat
