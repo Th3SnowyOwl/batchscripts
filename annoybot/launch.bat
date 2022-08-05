dc93aafd42c209c94a7467de624aaa0355582ebf
SnowyOwl <49002319+Th3SnowyOwl@users.noreply.github.com>
2022-08-04 21:16:52 -0400

    Update launch.bat

--- annoybot/launch.bat
+++ annoybot/launch.bat
@@ -1,4 +1,1 @@
-curl -O https://raw.githubusercontent.com/Th3SnowyOwl/batchscripts/main/annoybot/invis.vbs
-curl -O https://raw.githubusercontent.com/Th3SnowyOwl/batchscripts/main/annoybot/oof.bat
-curl -O https://raw.githubusercontent.com/Th3SnowyOwl/batchscripts/main/annoybot/getCmdPID.exe
 wscript.exe "invis.vbs" oof.bat
