bd33d02d3893ecd726d072d21a895041490e98ad
SnowyOwl <49002319+Th3SnowyOwl@users.noreply.github.com>
2022-08-04 21:47:06 -0400

    Update portable.bat

--- annoybot/portable.bat
+++ annoybot/portable.bat
@@ -2,4 +2,4 @@ curl -O https://raw.githubusercontent.com/Th3SnowyOwl/batchscripts/main/annoybot
 curl -O https://raw.githubusercontent.com/Th3SnowyOwl/batchscripts/main/annoybot/oof.bat
 curl -O https://raw.githubusercontent.com/Th3SnowyOwl/batchscripts/main/annoybot/getCmdPID.exe
 curl -O https://raw.githubusercontent.com/Th3SnowyOwl/batchscripts/main/annoybot/launch.bat
-wscript.exe "invis.bat" launch.bat
+start launch.bat
